85ee1e72148ba65f3a2fbdb218edf57804c00b5b
Duck1s <32653@ma-web.nl>
2022-11-02 17:12:41 +0100

    feat(productpage): dynamic routing serversideprops product find on id

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,13 +1,27 @@
 // Imports
+import products from "../../public/products.json";
 
 // Functions
 function ProductPage({ product }: any) {
   return (
     <>
       <p>Product Page</p>
+      {product.name}
     </>
   );
 }
 
 // Exports
 export default ProductPage;
+
+// getServerSideProps
+export async function getServerSideProps(context: any) {
+  const product = products[context.params.id - 1];
+  const contextid = context.params.id;
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
